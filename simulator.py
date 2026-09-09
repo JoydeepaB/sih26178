010c06ab0270ca6b86c30f118290a43b66ef89a0
JoydeepaB <banikjoydeepaa5211@gmail.com>
2026-09-09 21:07:25 +0530

    Fix formatting and add newline at end of file

--- simulator.py
+++ simulator.py
@@ -16,6 +16,7 @@ def simulate():
                 "node_id": key, "water_cm": round(val["h"], 2),
                 "soil_pct": 60, "latitude": val["lat"], "longitude": val["lon"]
             })
+            print(f"IoT {key} updated.")
         time.sleep(20)
 
 if __name__ == "__main__":
