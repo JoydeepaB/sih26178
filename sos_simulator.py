6f50b201522fcd79951110c4bfe8588ea7b0e3a5
JoydeepaB <banikjoydeepaa5211@gmail.com>
2026-09-09 20:21:52 +0530

    Uncomment send function call in main block

--- sos_simulator.py
+++ sos_simulator.py
@@ -7,7 +7,6 @@ def send():
     requests.post(URL, json={
         "device_id": "SIH_FIELD_USER_12", "lat": 23.835, "lon": 91.282
     })
-    print("SOS Alert visible on dashboard now!")
 
 if __name__ == "__main__":
     send()
